ac01c3dd0f0b6fcb5caa21db0f8bc1986bb12f7e
Jerry Tseng <0817jerry@gmail.com>
2018-10-09 15:29:36 +1100

    Add new file

---
+++ c_to_f.py
@@ -0,0 +1,7 @@
+# Converting different temperature from Celsius to Fahrenheit
+# User to enter celsius, return with fahrenheit
+
+c = input('Please enter Celsius figure: ')
+c = float (c)
+f = c * 9 / 5 + 32
+print('current temperature is : ', f)
